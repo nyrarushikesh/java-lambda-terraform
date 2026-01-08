2324f211fc814765084f5a24aa424a3b830b71e2
Rushikesh Savkar Thorave <rushikesh.thorave@zensar.com>
2026-01-08 16:22:33 +0530

    Initial commit

--- module/lambda/main.tf
+++ module/lambda/main.tf
@@ -29,15 +29,6 @@ resource "aws_lambda_function" "this" {
 resource "aws_lambda_function_url" "this" {
   function_name      = aws_lambda_function.this.function_name
   authorization_type = "NONE"
-
-  cors {
-    allow_credentials = true
-    allow_origins     = ["*"]
-    allow_methods     = ["*"]
-    allow_headers     = ["date", "keep-alive"]
-    expose_headers    = ["keep-alive", "date"]
-    max_age           = 86400
-  }
 }
 
 resource "aws_lambda_permission" "allow_function_url" {
