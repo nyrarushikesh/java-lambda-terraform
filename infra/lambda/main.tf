3b7d7d59e008ee29b1ba0c507c4fba63b8e61548
Rushikesh Savkar Thorave <rushikesh.thorave@zensar.com>
2026-01-08 14:39:15 +0530

    Initial commit

--- infra/lambda/main.tf
+++ infra/lambda/main.tf
@@ -1,8 +1,6 @@
 module "lambda" {
   source = "../../module/lambda"
 
-  bucket        = var.bucket
-  region        = var.region
   function_name = "hello-java"
   jar_path      = "../../app/hello-world/target/hello-lambda-1.0.jar"
 }
--- module/lambda/variables.tf
+++ module/lambda/variables.tf
@@ -1,18 +1,3 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-}
-
-variable "envname" {
-  description = "Environment name"
-  type        = string
-}
-
-variable "bucket" {
-  description = "Terraform backend bucket"
-  type        = string
-}
-
 variable "function_name" {
   description = "Lambda function name"
   type        = string
