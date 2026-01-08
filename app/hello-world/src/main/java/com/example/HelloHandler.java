ec225b8de1b3b96afe1423388b009f5a62a8382a
Rushikesh Savkar Thorave <rushikesh.thorave@zensar.com>
2026-01-08 15:21:01 +0530

    Initial commit

--- app/hello-world/src/main/java/com/example/HelloHandler.java
+++ app/hello-world/src/main/java/com/example/HelloHandler.java
@@ -2,12 +2,21 @@ package com.example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import java.util.HashMap;
 import java.util.Map;
 
-public class HelloHandler implements RequestHandler<Map<String, Object>, String> {
+public class HelloHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
     @Override
-    public String handleRequest(Map<String, Object> input, Context context) {
-        return "Hello World from Java Lambda!";
+    public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("statusCode", 200);
+        response.put("body", "Hello World from Java Lambda!");
+        
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "text/plain");
+        response.put("headers", headers);
+        
+        return response;
     }
 }
